340fdcbf6817f653c514b63ac46af00a2675426c
Tiago Lopes <tiagoltavares2002@gmail.com>
2022-08-13 17:00:33 -0300

    Adicionando teste de e-mail repetido

--- tests/cypress/e2e/signup.cy.js
+++ tests/cypress/e2e/signup.cy.js
@@ -1,17 +1,17 @@
 /// <reference types="cypress"/>
 
+const user = {
+    name: 'Tiago Lopes',
+    email: 'tiagoltavares2002@gmail.com',
+    password: 'pwd123'
+}
+
 describe('Cadastro de usuários', () => {
     beforeEach(() => {
         cy.visit('/signup')
     })
 
     it('Deve cadastrar um usuário com sucesso', () => {
-        const user = {
-            name: 'Tiago Lopes',
-            email: 'tiagoltavares2002@gmail.com',
-            password: 'pwd123'
-        }
-
         cy.task('removeUser', user.email)
 
         cy.get('input[placeholder=Nome]').type(user.name)
@@ -24,5 +24,18 @@ describe('Cadastro de usuários', () => {
             .find('p')
             .should('be.visible')
             .and('have.text', 'Agora você pode fazer seu login no Samurai Barbershop!')
+    })
+
+    it('Deve exibir um erro ao tentar cadastrar um usuário com e-mail repetido', () => {
+        cy.get('input[placeholder=Nome]').type(user.name)
+        cy.get('input[placeholder="E-mail"]').type(user.email)
+        cy.get('input[type=password]').type(user.password)
+
+        cy.contains('button', 'Cadastrar').click()
+
+        cy.get('.toast', { timeout: 8000 })
+            .find('p')
+            .should('be.visible')
+            .and('have.text', 'Email já cadastrado para outro usuário.')
     })
 })
