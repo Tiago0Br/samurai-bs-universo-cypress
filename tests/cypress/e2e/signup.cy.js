85bc7deb2fca0f68f5f1dd293e2d20e91e123e53
Tiago Lopes <tiagoltavares2002@gmail.com>
2022-08-20 16:52:27 -0300

    Alterando o teste de e-mail repetido
    - Remodelando massa de teste
    - Utilização do método "request" pra cadastrar o usuário via API
    - Criando contextos para os testes
    - Atualizando a versão do Cypress para a 10.6.0

--- tests/cypress/e2e/signup.cy.js
+++ tests/cypress/e2e/signup.cy.js
@@ -1,41 +1,59 @@
 /// <reference types="cypress"/>
 
-const user = {
-    name: 'Tiago Lopes',
-    email: 'tiagoltavares2002@gmail.com',
-    password: 'pwd123'
-}
-
 describe('Cadastro de usuários', () => {
     beforeEach(() => {
         cy.visit('/signup')
     })
 
-    it('Deve cadastrar um usuário com sucesso', () => {
-        cy.task('removeUser', user.email)
-
-        cy.get('input[placeholder=Nome]').type(user.name)
-        cy.get('input[placeholder="E-mail"]').type(user.email)
-        cy.get('input[type=password]').type(user.password)
-
-        cy.contains('button', 'Cadastrar').click()
-
-        cy.get('.toast', { timeout: 8000 })
-            .find('p')
-            .should('be.visible')
-            .and('have.text', 'Agora você pode fazer seu login no Samurai Barbershop!')
+    context('Cadastro válido', () => {
+        it('Deve cadastrar um usuário com sucesso', () => {
+            const user = {
+                name: 'Tiago Lopes',
+                email: 'tiagoltavares2002@gmail.com',
+                password: 'pwd123'
+            }
+    
+            cy.task('removeUser', user.email)
+    
+            cy.get('input[placeholder=Nome]').type(user.name)
+            cy.get('input[placeholder="E-mail"]').type(user.email)
+            cy.get('input[type=password]').type(user.password)
+    
+            cy.contains('button', 'Cadastrar').click()
+    
+            cy.get('.toast', { timeout: 8000 })
+                .find('p')
+                .should('be.visible')
+                .and('have.text', 'Agora você se tornou um(a) Samurai, faça seu login para ver seus agendamentos!')
+        })
     })
 
-    it('Deve exibir um erro ao tentar cadastrar um usuário com e-mail repetido', () => {
-        cy.get('input[placeholder=Nome]').type(user.name)
-        cy.get('input[placeholder="E-mail"]').type(user.email)
-        cy.get('input[type=password]').type(user.password)
-
-        cy.contains('button', 'Cadastrar').click()
-
-        cy.get('.toast', { timeout: 8000 })
-            .find('p')
-            .should('be.visible')
-            .and('have.text', 'Email já cadastrado para outro usuário.')
+    context('Tentativa de cadastro com dados inválidos', () => {
+        it('Deve exibir um erro ao tentar cadastrar um usuário com e-mail repetido', () => {
+            const user = {
+                name: 'João Lucas',
+                email: 'joaolucas@samuraibs.com',
+                password: 'pwd123',
+                is_provider: true
+            }
+    
+            cy.task('removeUser', user.email)
+            cy.request({
+                method: 'POST',
+                url: 'http://localhost:3333/users',
+                body: user
+            }).its('status').should('be.equal', 200)
+    
+            cy.get('input[placeholder=Nome]').type(user.name)
+            cy.get('input[placeholder="E-mail"]').type(user.email)
+            cy.get('input[type=password]').type(user.password)
+    
+            cy.contains('button', 'Cadastrar').click()
+    
+            cy.get('.toast', { timeout: 8000 })
+                .find('p')
+                .should('be.visible')
+                .and('have.text', 'Email já cadastrado para outro usuário.')
+        })
     })
 })
