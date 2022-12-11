e465bea89fd1c01a1fe7f1ecd1a64c111b1c1ba8
Tiago Lopes <tiagoltavares2002@gmail.com>
2022-12-10 21:42:50 -0300

    Criação de arquivo de Factory
    - Criando massa de testes no padrão Factory
    - Utilizando a biblioteca underscore para selecionar uma opção aleatória

--- tests/cypress/e2e/dash.cy.js
+++ tests/cypress/e2e/dash.cy.js
@@ -1,39 +1,23 @@
 /// <reference types="cypress"/>
 import loginPage from '../support/pages/login'
 import dashPage from '../support/pages/dash'
+import { customer, provider, appointment } from '../support/factories/dash'
 
 describe('Dashboard', () => {
     it('Deve exibir o agendamento feito por um cliente no dashboard', () => {
-        const data = {
-            customer: {
-                name: 'Nikki Sixx',
-                email: 'sixx@motleycure.com',
-                password: 'pwd123',
-                is_provider: false
-            },
-            provider: {
-                name: 'Ramon Valdes',
-                email: 'ramon@televisa.com',
-                password: 'pwd123',
-                is_provider: true
-            },
-            appointmentHour: '14:00'
-        }
-
-        const { customer, provider, appointmentHour } = data
         cy.postUser(customer)
         cy.postUser(provider)
 
         cy.apiLogin(customer)
         cy.setProviderId(provider.email)
-        cy.createAppointment(appointmentHour).then(() => {
+        cy.createAppointment(appointment.hour).then(() => {
             loginPage.go()
             loginPage.form(provider)
             loginPage.submit()
     
             dashPage.calendarShouldBeVisible()
             dashPage.selectDay(Cypress.env('appointmentDay'))
-            dashPage.appointmentShouldBe(customer, appointmentHour)
+            dashPage.appointmentShouldBe(customer, appointment.hour)
         })
     })
 })
